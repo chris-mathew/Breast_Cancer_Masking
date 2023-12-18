cd420b3e6bc616db5a2d1aa00955a5cfd8726315
cmm223icacuk <cmm223@ic.ac.uk>
2023-12-18 19:15:56 +0000

    - Added processing script used to process some of the images in the dataset - This is intended to be used as an example

---
+++ utility_scripts/Processing_script.py
@@ -0,0 +1,42 @@
+import pydicom
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import os
+
+
+def dicom_to_png(dicom_path, png_path):
+
+    dicom_data = pydicom.dcmread(dicom_path)
+
+    pixel_data = dicom_data.pixel_array
+    pixel_data = pixel_data - np.min(pixel_data)
+    pixel_data = pixel_data / np.max(pixel_data)
+
+    rgb_image = plt.cm.gray(pixel_data)
+    #Adding RGB channels to the image
+    rgb_image = (rgb_image[:, :, :3] * 255).astype(np.uint8)
+    #Saving 
+    Image.fromarray(rgb_image).save(png_path)
+
+
+def resize_image(path_in, size):
+    # Open the image file
+    original_image = Image.open(path_in)
+
+    # Resize the image
+    resized_image = original_image.resize(size)
+
+    # Save the resized image
+    resized_image.save(path_in)
+
+
+path = "C:/Users/chris/OneDrive - Imperial College London/CBIS Dataset/manifest-Egq0PU078220738724328010106/CBIS-DDSM" #PATH TO THE DICOM FILES
+folder_names = os.listdir(path)
+size_new = (320,320)
+
+for folder in folder_names:
+    input_path = path + '/' + folder
+    output_path = path + '/' + 'Reformatted' + '/' + folder[:-4] + '.png'  #PNG IMAGES ARE CREATED IN A FOLDER CALLED "Reformatted"
+    dicom_to_png(input_path, output_path) #DICOM is convere
+    resize_image(output_path, size_new)
