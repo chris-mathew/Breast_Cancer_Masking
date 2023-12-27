66641f6fdf7dbc8ff9ed17fc4b5c4466237e7b72
cmm223icacuk <cmm223@ic.ac.uk>
2023-12-27 17:35:46 +0000

    - Added group_id to group the images

--- sql_database/Example_Scripts/Upload_Example.py
+++ sql_database/Example_Scripts/Upload_Example.py
@@ -10,29 +10,33 @@ sql = SqlConnect(server="ctrl-alt-elite.database.windows.net", database="ai_bres
                  password="Tsnte7TF6nMZTPY")
 
 sql.connect()
-path = "C:/Users/chris/OneDrive - Imperial College London/CBIS Dataset/manifest-Egq0PU078220738724328010106/CBIS-DDSM/Grouped_nodir"
+path = "C:/Users/chris/OneDrive - Imperial College London/CBIS Dataset/manifest-Egq0PU078220738724328010106/CBIS-DDSM/Grouped"
 
 folder_names = os.listdir(path)
 values = []
-id = 0
+group_id = 0
 
-for folder in folder_names:
-    folder_value = {}
-    with open(path+"/"+folder, 'rb') as file:
-        folder_value['pixel_data'] = file.read()
-    image_name_split = folder.split(".")[0].split("_")
-    folder_value['patient_id'] = int(image_name_split[1])
-    if image_name_split[2] == 'LEFT':
-        folder_value['direction'] = 0
-    else:
-        folder_value['direction'] = 1
-    if image_name_split[3] == "CC":
-        folder_value['image_view'] = 0
-    else:
-        folder_value['image_view'] = 1
-    folder_value['density'] = int(image_name_split[4])
+for dir in folder_names:
+    group_id+=1
+    dirnames = os.listdir(path+"/"+dir)
+    for folder in dirnames:
+        folder_value = {}
+        folder_value['group_id'] = group_id
+        with open(path+"/"+dir+'/'+folder, 'rb') as file:
+            folder_value['pixel_data'] = file.read()
+        image_name_split = folder.split(".")[0].split("_")
+        folder_value['patient_id'] = int(image_name_split[1])
+        if image_name_split[2] == 'LEFT':
+            folder_value['direction'] = 0
+        else:
+            folder_value['direction'] = 1
+        if image_name_split[3] == "CC":
+            folder_value['image_view'] = 0
+        else:
+            folder_value['image_view'] = 1
+        folder_value['density'] = int(image_name_split[4])
 
-    sql.insert("dbo.ddsm_dataset", [folder_value])
+        sql.insert("dbo.ddsm_dataset", [folder_value])
 
 
 sql.close()
